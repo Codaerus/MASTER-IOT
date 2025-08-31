4ca86fff7f3d51ae41964821c7817d27944317d3
Codaerus <christianqc91@gmail.com>
2025-08-31 13:16:12 -0500

    Create code_6.ino

---
+++ CLASE4/code_6.ino
@@ -0,0 +1,62 @@
+#include <PubSubClient.h>
+#include <WiFi.h>
+/********* DHT *********************/
+#include <DHT.h>
+DHT dht(2,DHT11);
+char msg[30];
+/***********************************/
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long int t = 0;
+
+void setup() {
+  setup_wifi();
+  client.setServer("18.189.7.185", 1883);
+  client.setCallback(callback);
+  ledcAttach(4,500,8);
+  /****************  DHT ***************/
+  dht.begin();
+  /*************************************/
+}
+
+void callback(const char *topic, byte* payload, unsigned int length){
+  payload[length] = '\0';
+  String val  = String((char*)payload);
+  Serial.println(val);
+  int duty = val.toInt();
+  ledcWrite(4,duty);
+}
+
+void reconnect(){
+  if(client.connect("ESP32-1")){
+    Serial.println("MQTT OK");
+    client.subscribe("canalPWM");
+  }
+}
+
+void loop() {
+  if(!client.connected()){
+    reconnect();
+  }
+  client.loop();
+  if(millis()-t>10000){
+    /************ DHT   *****************/
+    int temp = dht.readTemperature();
+    int humi = dht.readHumidity();
+    snprintf(msg,30,"%d,%d",temp,humi);
+    Serial.println(msg);
+    /************************************/
+    client.publish("canalx",msg);
+    t = millis();
+  }
+}
+
+void setup_wifi(){
+  Serial.begin(9600);
+  WiFi.begin("MOVISTAR_78A8","NS2ajtQJ7TtDt9m"); //DHCP
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println(WiFi.localIP());
+}
