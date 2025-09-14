26598f6790d751229678d361231d1055b920c713
Codaerus <christianqc91@gmail.com>
2025-09-14 19:25:37 +0300

    Update pub_mqtt_gps.ino

--- CLASE6/pub_mqtt_gps.ino
+++ CLASE6/pub_mqtt_gps.ino
@@ -1,51 +1,49 @@
-#include <WiFi.h>
 #include <PubSubClient.h>
-/************* GPS  ****************/
+#include <WiFi.h>
+/***************** GPS ******************/
 #include <TinyGPSPlus.h>
 TinyGPSPlus gps;
-/***********************************/
+/****************************************/
 WiFiClient espClient;
 PubSubClient client(espClient);
+unsigned long int t = 0;
 char msg[30];
 
 void setup() {
   setup_wifi();
-  client.setServer("18.224.38.115",1883);
-  /************** SERIAL2 *******************/
-  Serial2.begin(9600,SERIAL_8N1,16,17);
-  /******************************************/
+  client.setServer("3.20.224.30", 1883);
+  Serial2.begin(9600,SERIAL_8N1,16,17); //GPS
 }
-
 void reconnect(){
-  if(client.connect("ESP32")){
+  if(client.connect("ESP32-1")){
     Serial.println("MQTT OK");
   }
 }
 
 void loop() {
   if(!client.connected()){
-      reconnect();
-    }
-  /*******************************/
-    if(Serial2.available()){
-      char c = Serial2.read();
-      if (gps.encode(c)){
-          float lati = gps.location.lat();
-          float longi = gps.location.lng();
-          snprintf(msg,30,"%f,%f",lati,longi);
-          client.publish("canalx",msg);
-          delay(4000);
-        }
-   }
+    reconnect();
   }
-
+  if(Serial2.available()){
+    char c = Serial2.read();
+    if (gps.encode(c)){
+      if(millis()-t>10000){
+        float lati = gps.location.lat();
+        float longi = gps.location.lng();
+        snprintf(msg,30,"%.6f,%.6f",lati,longi);
+        client.publish("GPS",msg);
+        t = millis();
+      }
+    }
+  }
+}
 
 void setup_wifi(){
   Serial.begin(9600);
-  WiFi.begin("MOVISTAR_78A8","NS2ajtQJ7TtDt9m"); //SSID, PASS
+  WiFi.begin("MOVISTAR_78A8","NS2ajtQJ7TtDt9m"); //DHCP
   while(WiFi.status() != WL_CONNECTED){
-    delay(300);
     Serial.print(".");
+    delay(300);
   }
-  Serial.println( WiFi.localIP());
+  Serial.println(WiFi.localIP());
 }
